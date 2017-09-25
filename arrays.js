ce2119aecaec5061d337f0269011f2683a72f8fd
Cheney Shreve <cheney.shreve@gmail.com>
2017-09-25 19:00:20 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,9 +7,9 @@ chocolateBars = [
 
 
 function addElementToBeginningOfArray(array,x) {
- return [x...array]
+ return [x,...array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array,x){
-  return array.push(x)
+  return array.unshift(x)
 }
